1158c35a33c0379871ac132b393853b8dc700b28
Roshun Jones <jonesrw@u.washington.edu>
2016-11-16 23:36:34 -0800

    Initial pass at classifier

--- src/Classifier.py
+++ src/Classifier.py
@@ -1,12 +1,40 @@
 from nltk.corpus import stopwords
+from MessageDAO import MessageDAO
+import nltk.classify
 
 
 class Classifier:
     def __init__(self):
-        print 'Init'
+        self.training_features = None
+
+    # Feature Extraction
+    def extract_features(self, message, training_features):
+            features = {}
+            words = set(message)
+            for word in training_features:
+                features[word] = (word in words)
+            return features
 
     def train(self):
-        print 'Training'
+        # Get training messages
+        message_dao = MessageDAO()
+
+        training_messages = message_dao.retrieve_training_messages()
+
+        # Preprocess training messages
+        training_messages = self.__preprocess(training_messages)
+
+        # Get training features
+        self.training_features = self.__generate_word_features(training_messages)
+
+        training_set = nltk.classify.apply_features(self.extract_features, training_messages)
+
+        classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+        # Get Tesing Messages
+        testing_messages = message_dao.retrieve_testing_messages()
+
+        print 'Accuracy:', nltk.classify.accuracy(classifier, testing_messages)
 
     def classify(self):
         print 'Classify'
@@ -25,18 +53,44 @@ class Classifier:
 
     # Helper method that generates word features based on input
     # Input: List of tuples(List of words, sentiment for these words)
-    def __generate_word_features(self, input):
-        print
+    def __generate_word_features(self, messages):
+        temp = []
+
+        for (messages, sentiment) in messages:
+            temp.extend(messages)
+
+        return list(set(temp))
 
     # Helper method that tokenizes input
     # Input: List of tuples(Tweet message, sentiment)
-    def __tokennize_input(self, input):
-        print
+    def __tokenize_input(self, messages):
+        tokenized_messages = []
+        for message, sentiment in messages:
+            # Transform list to tokenized list and lower case
+            words = [word.lower() for word in message.split()]
+            tokenized_messages.append((words, sentiment))
+        return tokenized_messages
+
+    # Preprocessing method
+    def __preprocess(self, messages):
+        # Tokenize messages
+        processed_messages = self.__tokenize_input(messages)
+
+        return processed_messages
+
+
+    # Feature Extraction
+    def extract_features(self, message):
+        features = {}
+        words = set(message)
+        for word in self.training_features:
+            features['contains(%s)' % word] = (word in words)
+        return features
+
 
 def main():
-    A = ['we', 'must', 'win']
     classifier = Classifier()
-    print classifier.remove_stop_words(A)
+    classifier.train()
 
 if __name__ == '__main__':
     main()
