9dc45771a0926c00a640aa7ba45fad0f95163bf7
Roshun Jones <jonesrw@u.washington.edu>
2016-12-03 10:21:39 -0800

    Fixed training and classify code

--- src/Classifier.py
+++ src/Classifier.py
@@ -2,12 +2,15 @@ from nltk.corpus import stopwords
 from MessageDAO import MessageDAO
 import nltk.classify
 import pickle
+import re
+import random
 
 
 class Classifier:
     def __init__(self):
         self.training_features = []
         self.testing_features = []
+        self.word_features = []
         nltk.download("stopwords")
         self.classifier = self.get_classifier()
 
@@ -16,7 +19,7 @@ class Classifier:
         if not self.classifier:
             return
 
-        features = self.__generate_word_features(message.split())
+        features = self.__extract_features(message.split())
         print self.classifier.classify(features);
 
     def get_classifier(self):
@@ -31,7 +34,7 @@ class Classifier:
 
         return classifier
 
-    def test(self, classifier):
+    def test(self):
         print 'Acquiring testing messages...'
 
         message_dao = MessageDAO()
@@ -43,26 +46,11 @@ class Classifier:
         print 'Generating test features'
 
         # Get testing features
-        positive_words = [word for words, sentiment in testing_messages for word in words if sentiment == '4']
-        negative_words = [word for words, sentiment in testing_messages for word in words if sentiment == '0']
-        neutral_words = [word for words, sentiment in testing_messages for word in words if sentiment == '2']
-
-        self.testing_features.append((self.__generate_word_features(positive_words), '4'))
-        self.testing_features.append((self.__generate_word_features(negative_words), '0'))
-        self.testing_features.append((self.__generate_word_features(neutral_words), '2'))
+        self.word_features = self.__generate_word_features(self.__get_all_words(testing_messages))
+        self.testing_features = nltk.classify.apply_features(self.__extract_features, testing_messages)
 
         print 'Testing...'
-        print 'Accuracy:', nltk.classify.util.accuracy(classifier, self.testing_features)
-
-    # Using a Bag of Words Model, extract features and return dictionary of features
-    # Input: List of words
-    def __generate_word_features(self, words):
-        # remove stop words
-        words = self.__remove_stop_words(words)
-        features = {}
-        for word in words:
-            features[word] = True
-        return features
+        print 'Accuracy:', nltk.classify.util.accuracy(self.classifier, self.testing_features)
 
     def __train(self):
         print 'Acquiring training messages...'
@@ -73,18 +61,14 @@ class Classifier:
         training_messages = message_dao.retrieve_training_messages()
 
         # Pre-process training messages
-        training_messages = self.__preprocess(training_messages)[:1000]
+        training_messages = self.__preprocess(training_messages)[:200]
 
         print 'Generating training features...'
 
         # Get training features
-        positive_words = [word for words, sentiment in training_messages for word in words if sentiment == '4']
-        neutral_words = [word for words, sentiment in training_messages for word in words if sentiment == '2']
-        negative_words = [word for words, sentiment in training_messages for word in words if sentiment == '0']
+        self.word_features = self.__generate_word_features(self.__get_all_words(training_messages))
+        self.training_features = nltk.classify.apply_features(self.__extract_features, training_messages)
 
-        self.training_features.append((self.__generate_word_features(positive_words),'4'))
-        self.training_features.append((self.__generate_word_features(negative_words), '0'))
-        self.training_features.append((self.__generate_word_features(neutral_words), '2'))
         print 'Training...'
 
         classifier = nltk.NaiveBayesClassifier.train(self.training_features)
@@ -97,10 +81,8 @@ class Classifier:
 
         return classifier
 
-
     # Removes stop words from list of words inputed.
     # Utilizes corpus provided by NLTK
-    # Input: List of tuples(List of words, sentiment)
     def __remove_stop_words(self, words):
         # Create Set of stop words
         stop_words = set(stopwords.words('english'))
@@ -115,6 +97,9 @@ class Classifier:
     def __tokenize_input(self, messages):
         tokenized_messages = []
         for message, sentiment in messages:
+            # clean message before tokenizing
+            message = self.__clean(message)
+
             # Transform list to tokenized list and lower case
             words = [word.lower() for word in message.split()]
             tokenized_messages.append((words, sentiment))
@@ -124,12 +109,46 @@ class Classifier:
     def __preprocess(self, messages):
         # Tokenize messages
         processed_messages = self.__tokenize_input(messages)
-
         return processed_messages
+
+    def __clean(self, message):
+        words = []
+
+        for word in message.split():
+            if re.search(r'@\w', word) or re.search(r'http:', word) or re.search(r'\d', word) or len(word) < 3:
+                continue
+
+            word = re.sub(r'[.?!\,()#-+$^%;-]*', '', word)
+            words.append(word)
+
+        return ' '.join(words)
+
+    def __get_all_words(self, messages):
+        words = []
+        for (words, sentiment) in messages:
+            words.extend(words)
+
+        return self.__remove_stop_words(words)  # remove stop words
+
+    def __generate_word_features(self, words):
+        words = nltk.FreqDist(words)
+        word_features = words.keys()
+
+        return word_features
+
+    def __extract_features(self, message):
+        words = set(message)
+        features = {}
+
+        for word in self.word_features:
+            features[word] = (word in words)
+
+        return features
 
 
 def main():
     classifier = Classifier()
-    classifier.classify("I love snow")
+    classifier.test()
+    classifier.classify("I hate snow")
 if __name__ == '__main__':
     main()
